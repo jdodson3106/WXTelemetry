cf6756f9378ffc8828f242b0930cc7e47400a37d
Justin Dodson <blackflamewebdev@gmail.com>
2017-06-16 06:11:29 -0500

    created serial port selector to read in and chart live serial data

--- src/WX/Controller.java
+++ src/WX/Controller.java
@@ -1,15 +1,13 @@
 package WX;
 
 import com.fazecast.jSerialComm.SerialPort;
+import javafx.application.Platform;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.chart.AreaChart;
-import javafx.scene.chart.LineChart;
-import javafx.scene.chart.NumberAxis;
-import javafx.scene.chart.XYChart;
+import javafx.scene.chart.*;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
@@ -17,18 +15,21 @@ import javafx.scene.control.MenuItem;
 import javafx.scene.layout.BorderPane;
 
 import java.net.URL;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.ResourceBundle;
 import java.util.Scanner;
 
 public class Controller implements Initializable{
 
 
-    private final NumberAxis xAxis = new NumberAxis();
+    private final CategoryAxis xAxis = new CategoryAxis();
     private final NumberAxis yAxis = new NumberAxis();
     private AreaChart areaChart;
     private LineChart lineChart;
-
+    private XYChart.Series<String, Number> temperature = new XYChart.Series<String, Number>();
+    private XYChart.Series<String, Number> pressure = new XYChart.Series<String, Number>();
     @FXML
     private ComboBox menuComSelect;
     @FXML
@@ -62,13 +63,13 @@ public class Controller implements Initializable{
     public void selectChartType(ActionEvent event) {
         if (chartSelection.getValue().equals("Area Chart")) {
             areaChart = createAreaChart();
-            populateAreaChartData();
+            //populateAreaChartData();
             borderPane.setCenter(areaChart);
 
         }
         else if (chartSelection.getValue().equals("Line Chart")) {
             lineChart = createLineChart();
-            populateLineChartData();
+           // populateLineChartData();
             borderPane.setCenter(lineChart);
 
         }
@@ -83,18 +84,22 @@ public class Controller implements Initializable{
     /*
      *   Creates new area chart.
      */
-    private AreaChart<Number, Number> createAreaChart() {
-        AreaChart<Number, Number> areaChart = new AreaChart<Number, Number>(xAxis, yAxis);
+    private AreaChart<String, Number> createAreaChart() {
+        AreaChart<String, Number> areaChart = new AreaChart<String, Number>(xAxis, yAxis);
         areaChart.setTitle("Atmospheric Data");
+        borderPane.setCenter(areaChart);
         return areaChart;
     }
 
     /*
      *   Creates new line chart.
      */
-    private LineChart<Number, Number> createLineChart() {
-        LineChart<Number, Number> lineChart = new LineChart<Number, Number>(xAxis, yAxis);
+    private LineChart<String, Number> createLineChart() {
+        LineChart<String, Number> lineChart = new LineChart<String, Number>(xAxis, yAxis);
         lineChart.setTitle("Atmospheric Data");
+        xAxis.setLabel("Current Time");
+        yAxis.setLabel("Measurements");
+        borderPane.setCenter(lineChart);
         return lineChart;
     }
 
@@ -110,67 +115,20 @@ public class Controller implements Initializable{
 
 
     /*
-     * populates the chart with dummy data.
-     * TODO: add chart data upon inflow of Serial data.
+     * populates charts with dummy data
      */
     @FXML
-    public void populateAreaChartData() {
-        XYChart.Series<Number, Number> temperature = new XYChart.Series<Number, Number>();
-        temperature.setName("Temperature (F)");
-        temperature.getData().add(new XYChart.Data<Number, Number>(0, 90));
-        temperature.getData().add(new XYChart.Data<Number, Number>(2, 99));
-        temperature.getData().add(new XYChart.Data<Number, Number>(3, 100));
-        temperature.getData().add(new XYChart.Data<Number, Number>(4, 105));
-        temperature.getData().add(new XYChart.Data<Number, Number>(5, 97));
-        temperature.getData().add(new XYChart.Data<Number, Number>(6, 92));
-        temperature.getData().add(new XYChart.Data<Number, Number>(7, 91));
-        temperature.getData().add(new XYChart.Data<Number, Number>(8, 88));
-        temperature.getData().add(new XYChart.Data<Number, Number>(9, 82));
-
-
-        XYChart.Series<Number, Number> dewPoint = new XYChart.Series<Number, Number>();
-        dewPoint.setName("Dew Point");
-        dewPoint.getData().add(new XYChart.Data<Number, Number>(0, 70));
-        dewPoint.getData().add(new XYChart.Data<Number, Number>(2, 72));
-        dewPoint.getData().add(new XYChart.Data<Number, Number>(3, 77));
-        dewPoint.getData().add(new XYChart.Data<Number, Number>(4, 78));
-        dewPoint.getData().add(new XYChart.Data<Number, Number>(5, 72));
-        dewPoint.getData().add(new XYChart.Data<Number, Number>(6, 71));
-        dewPoint.getData().add(new XYChart.Data<Number, Number>(7, 77));
-        dewPoint.getData().add(new XYChart.Data<Number, Number>(8, 79));
-        dewPoint.getData().add(new XYChart.Data<Number, Number>(9, 70));
-
-        areaChart.getData().addAll(temperature, dewPoint);
+    public void populateAreaChartData(XYChart.Series pressureReading, XYChart.Series altitudeReading) {
+        pressureReading.setName("Pressure (mBar)");
+        altitudeReading.setName("Altitude");
+        areaChart.getData().addAll(pressureReading, altitudeReading);
     }
 
     @FXML
-    public void populateLineChartData() {
-        XYChart.Series<Number, Number> temperature = new XYChart.Series<Number, Number>();
-        temperature.setName("Temperature (F)");
-        temperature.getData().add(new XYChart.Data<Number, Number>(0, 90));
-        temperature.getData().add(new XYChart.Data<Number, Number>(2, 99));
-        temperature.getData().add(new XYChart.Data<Number, Number>(3, 100));
-        temperature.getData().add(new XYChart.Data<Number, Number>(4, 105));
-        temperature.getData().add(new XYChart.Data<Number, Number>(5, 97));
-        temperature.getData().add(new XYChart.Data<Number, Number>(6, 92));
-        temperature.getData().add(new XYChart.Data<Number, Number>(7, 91));
-        temperature.getData().add(new XYChart.Data<Number, Number>(8, 88));
-        temperature.getData().add(new XYChart.Data<Number, Number>(9, 82));
-
-
-        XYChart.Series<Number, Number> dewPoint = new XYChart.Series<Number, Number>();
-        dewPoint.setName("Dew Point");
-        dewPoint.getData().add(new XYChart.Data<Number, Number>(0, 70));
-        dewPoint.getData().add(new XYChart.Data<Number, Number>(2, 72));
-        dewPoint.getData().add(new XYChart.Data<Number, Number>(3, 77));
-        dewPoint.getData().add(new XYChart.Data<Number, Number>(4, 78));
-        dewPoint.getData().add(new XYChart.Data<Number, Number>(5, 72));
-        dewPoint.getData().add(new XYChart.Data<Number, Number>(6, 71));
-        dewPoint.getData().add(new XYChart.Data<Number, Number>(7, 77));
-        dewPoint.getData().add(new XYChart.Data<Number, Number>(8, 79));
-        dewPoint.getData().add(new XYChart.Data<Number, Number>(9, 70));
-
-        lineChart.getData().addAll(temperature, dewPoint);
+    public void populateLineChartData(XYChart.Series pressureReading, XYChart.Series altitudeReading) {
+        pressureReading.setName("Pressure (mBar)");
+        altitudeReading.setName("Altitude");
+        lineChart.getData().addAll(pressureReading, altitudeReading);
     }
 
     public void showAlert() {
@@ -197,34 +155,84 @@ public class Controller implements Initializable{
      */
     @FXML
     public void selectPort(ActionEvent event) {
-        //TODO: Select serial port based on the combobox selection.
+        // create a serial port object based on what port the user selects
+        final SerialPort chosenPort = SerialPort.getCommPort(menuComSelect.getValue().toString());
+
+        // Check to see if the connect button text is equal to connect when clicked.
         if(connectButton.getText().equals("Connect")) {
-            // Assign selected port and try to connect
-            final SerialPort chosenPort = SerialPort.getCommPort(menuComSelect.getValue().toString());
+
+            // configure port timeouts.
             chosenPort.setComPortTimeouts(SerialPort.TIMEOUT_SCANNER, 0, 0);
             if (chosenPort.openPort()) {
+                /*
+                    *if the port opens, then change text, disable the dropdown,
+                    * create new line chart, and create a new scanner to read in serial data.
+                 */
                 connectButton.setText("Disconnect");
                 menuComSelect.setDisable(true);
-
+                // TODO: create chart type based on user selection;
+                final LineChart chart = createLineChart();
+                temperature.setName("Pressure (mBar)");
+                pressure.setName("Altitude");
                 // Create a new thread that will listen for incoming data and populate the graph
-                Thread thread = new Thread(){
+                final Scanner inputStream = new Scanner(chosenPort.getInputStream());
+
+                // Set up a new runnable task.
+                Runnable task = new Runnable() {
                     @Override
                     public void run() {
-                        Scanner inputStream = new Scanner(chosenPort.getInputStream());
-                        while (inputStream.hasNextLine()) {
-                            String line1 = inputStream.nextLine();
-                            String line2 = inputStream.nextLine();
-                            int pressure = Integer.parseInt(line1);
-                            int altitude = Integer.parseInt(line2);
-                            // TODO: Add data to chart series to be populated
-                        }
-                    }
-                };
-                thread.run();
-            } else {
-                // disconnect from the port
-            }
+                        while (inputStream.hasNextLine() && connectButton.getText().equals("Disconnect")) {
+                            // put the thread to sleep for 1 second so the chart can update in sync with the incoming data.
+                            try {
+                                Thread.sleep(1000);
+                            } catch (Exception e) {
+                                e.printStackTrace();
+                            }
+                            // create new date and format objects to setup for the chart xAxis.
+                            final Date now = new Date();
+                            final SimpleDateFormat formatter = new SimpleDateFormat("hh:mm:ss");
 
+                            // runLater to ensure the UI maintains usability while reading in serial port data on another thread
+                            Platform.runLater(new Runnable() {
+                                @Override
+                                public void run() {
+                                    /*
+                                        * grab the first two lines of data, and assign them to variables and then
+                                        * convert them to ints and add to the two different series.
+                                        * Add the series to the chart.
+                                        * NOTE: this is in a try/catch because sometimes the initial data
+                                        *       can be corrupt. This allows us to catch that corrupt data
+                                        *       and not do anything with the exception so we can discard it
+                                        *       and move on to the next lines of incoming data.
+                                     */
+                                    try{
+                                        String line1 = inputStream.nextLine();
+                                        String line2 = inputStream.nextLine();
+                                        final int pressureOutput = Integer.parseInt(line1);
+                                        final int altitudeOutput = Integer.parseInt(line2);
+                                        temperature.getData().add(new XYChart.Data<String, Number>(formatter.format(now), pressureOutput));
+                                        pressure.getData().add(new XYChart.Data<String, Number>(formatter.format(now), altitudeOutput));
+                                        chart.getData().addAll(temperature, pressure);
+                                    } catch (Exception e){};
+
+                                }
+                            });
+
+                        }
+                        // close the Scanner object from reading in.
+                        inputStream.close();
+                    }
+
+                };
+                // create new Thread, pass in the Runnable object, and then start the thread.
+                new Thread(task).start();
+            }
+        }
+        else if (connectButton.getText().equals("Disconnect")){
+            // disconnect from the port
+            chosenPort.closePort();
+            connectButton.setText("Connect");
+            menuComSelect.setDisable(false);
         }
     }
 
